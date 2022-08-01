64eb6c77d57f774fc998792b6a87c7377f46e452
redflame <vanya133719@gmail.com>
2022-08-01 17:25:08 +0300

    discovery microservice module

--- core/src/main/java/com/makichanov/core/CoreApplication.java
+++ core/src/main/java/com/makichanov/core/CoreApplication.java
@@ -2,12 +2,14 @@ package com.makichanov.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableTransactionManagement
+@EnableEurekaClient
 public class CoreApplication {
 
     public static void main(String[] args) {
